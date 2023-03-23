07d9062b3519d4da035d900140544f9ea0dee037
A10-warthog <behemoth11111111@gmail.com>
2023-03-24 00:00:04 +0530

    Rewrite the code

--- cash-register.js
+++ cash-register.js
@@ -1,5 +1,9 @@
 export default checkCashRegister(price, cash, cid) {
-    let change = cid.reduce((obj, val) => {
-    const change = cash - price;
-  });
+  let change = cash - price;  
+  for (let i = 0; i < cid.length; i++) {
+     change = change - cid[i][1];
+     cid[i][1] = 0;
+     if (change === 0 && cid.every(elm => elm[1] === 0))
+      return change
+  }
 }
