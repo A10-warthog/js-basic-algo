ae67b1ffd5c5cf0170b94c66bea8b8f6444e5d99
A10-warthog <behemoth11111111@gmail.com>
2023-03-25 21:03:15 +0530

    Return an object when change is equal ot totalCash

--- cash-register.js
+++ cash-register.js
@@ -1,15 +1,30 @@
 export default checkCashRegister(price, cash, cid) {
-  const currency = { 
-    Penny: 0.01,
-    Nickel: 0.05,
-    Dime: 0.1,
-    Quarter: 0.25,
-    'One Dollar': 1,
-    'Five Dollars': 5,
-    'Ten Dollars': 10,
-    'Twenty Dollars': 20,
-    'Hundred Dollars': 100,
+   // fixed value to dollar
+  const currency = {
+    PENNY: 0.01,
+    NICKEL: 0.05,
+    DIME: 0.1,
+    QUARTER: 0.25,
+    ONE: 1,
+    FIVE: 5,
+    TEN: 10,
+    TWENTY: 20,
+    "ONE HUNDRED": 100,
   };
-  let change = cash - price;  
+  // change all values to pennies
+  for (let val in currency) currency[val] = currency[val] * 100;
+
+  // get change
+  let change = cash * 100 - price * 100;
+
+  // get total cash in register
+  const totalCash = cid.reduce((addTo, val) => addTo + val[1], 0) * 100;
+
+  // return obj with status closed and cid
+  if (change === totalCash)
+    return {
+      status: "CLOSED",
+      change: cid,
+    };  
   
 }
